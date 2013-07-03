164278e32c6de79983be3b49350f039ab6e1459c
andi-neck <andi.neck@intesso.com>
2013-07-03 18:01:04 +0200

    use events if running on server

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var Emitter = require('emitter');
+if ('undefined' == typeof window) {
+  module.exports = require('events');
+} else {
+  var Emitter = require('emitter');
+  
+  // alias all the things!
+  Emitter.prototype.addListener = Emitter.prototype.on;
+  Emitter.prototype.removeListener = Emitter.prototype.off;
+  Emitter.prototype.removeAllListeners = Emitter.prototype.off;
+  
+  Emitter.prototype.setMaxListeners = function(){ /* noop */ };
+  
+  module.exports.EventEmitter = Emitter;
+}
 
-// alias all the things!
-Emitter.prototype.addListener = Emitter.prototype.on;
-Emitter.prototype.removeListener = Emitter.prototype.off;
-Emitter.prototype.removeAllListeners = Emitter.prototype.off;
 
-Emitter.prototype.setMaxListeners = function(){ /* noop */ };
-
-exports.EventEmitter = Emitter;
