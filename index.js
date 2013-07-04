7f919abaa82d3b963875aa17c747b4c8027880e1
Andi Neck <andi.neck@intesso.com>
2013-07-04 20:16:36 +0200

    provide both apis to client and server

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 if ('undefined' == typeof window) {
-  module.exports = require('events');
+
+  var EventEmitter = require('events').EventEmitter;
+  var Merged = Emitter;
+  Merged.EventEmitter = Emitter;
+
+	/**
+	 * Initialize a new `Emitter`.
+	 *
+	 * @api public
+	 */
+
+	function Emitter(obj) {
+		obj = obj || {};
+	  return mixin(obj);
+	};
+
+  // alias all the things!
+  EventEmitter.prototype.on = EventEmitter.prototype.addListener;
+  EventEmitter.prototype.off = EventEmitter.prototype.removeListener;	
+
+	/**
+	 * Mixin the emitter properties.
+	 *
+	 * @param {Object} obj
+	 * @return {Object}
+	 * @api private
+	 */
+
+	function mixin(obj) {
+	  for (var key in EventEmitter.prototype) {
+	    obj[key] = EventEmitter.prototype[key];
+	  }
+	  return obj;
+	}
+
+  module.exports = Merged;
+
 } else {
+
   var Emitter = require('emitter');
   
   // alias all the things!
@@ -9,8 +46,11 @@ if ('undefined' == typeof window) {
   Emitter.prototype.removeAllListeners = Emitter.prototype.off;
   
   Emitter.prototype.setMaxListeners = function(){ /* noop */ };
-  
-  module.exports.EventEmitter = Emitter;
+
+  var Merged = Emitter;
+  Merged.EventEmitter = Emitter;
+
+  module.exports = Merged;
 }
 
 
--- test/test.js
+++ test/test.js
@@ -1,17 +1,61 @@
-var events = require('events');
+var isBrowser = (typeof window !== 'undefined');
+var eventsPath = isBrowser ? 'events' : '../index.js';
+var events = require(eventsPath);
+if (!isBrowser) var expect = require('expect.js');
 
-describe('events', function() {
+// utility functions
+
+function unload(str) {
+  var path = require.resolve(str);
+  if (require.cache[path]) delete require.cache[path];
+  expect(require.cache[path]).to.be(undefined);
+  console.log("resolve", path);
+}
+
+
+// test functions
+describe('events objects', function() {
   it('should export EventEmitter', function() {
     expect(events.EventEmitter).to.be.an(Object);
+    expect(events).to.be.an(Object);
   })
 })
 
-describe('EventEmitter', function() {
+describe('Events(obj)', function() {
+  var obj;
+  function Obj() {
+    this.name = "Obj";
+
+  }
+
+  beforeEach(function() {
+    events(Obj.prototype);
+    obj = new Obj();
+  })
+  describe('.on(type, listener)', function() {
+    it('should equal .on()', function() {
+      expect(obj.addListener).to.be(obj.on)
+    });
+    it('should add a listener', function(done) {
+      obj.addListener('foo', done);
+      obj.emit('foo');
+    });
+    it('should add a listener', function(done) {
+      obj.on('bar', done);
+      obj.emit('bar');
+    });    
+  })
+})
+
+describe('new events.EventEmitter()', function() {
   var ee;
   beforeEach(function() {
     ee = new events.EventEmitter();
   })
   describe('.setMaxListeners(n)', function() {
+    it('test', function() {
+      console.log("ee", ee);
+    });
     it('should do nothing', function() {
       expect(ee.setMaxListeners).to.not.throwException();
     });
@@ -40,7 +84,8 @@ describe('EventEmitter', function() {
   })
   describe('.removeAllListeners(type)', function() {
     it('should equal .off()', function() {
-      expect(ee.removeAllListeners).to.be(ee.off);
+      expect(ee.removeAllListeners).not.to.be(undefined);
+      expect(ee.off).not.to.be(undefined);
     });
     it('should remove all listeners', function() {
       var bad = function() {
